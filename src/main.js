dffd8c20e10b77fe791b3fb8fe4e5502fb920bb3
duhuiqiang <1915437779@qq.com>
2019-11-22 21:16:40 +0800

    dhq

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import Api from "./api/http.js"; //导入api接口
+import Cube from "cube-ui";
 
+Vue.use(Cube);
 Vue.prototype.$api = Api; //在vue上挂载api
 
 new Vue({
